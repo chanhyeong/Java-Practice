182d6ada3187b107d6fdcda1b06984908db05bc5
chanhyeong <chu7825@gmail.com>
2018-12-16 14:49:32 +0900

    apply lombok

--- src/main/java/io/github/chanhyeong/Main.java
+++ src/main/java/io/github/chanhyeong/Main.java
@@ -4,7 +4,7 @@ import io.github.chanhyeong.common.BaseExecutor;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    Class<?> cls = Class.forName("io.github.chanhyeong.workspace.hyeong.ISSUE101_insertNewItem");
+    Class<?> cls = Class.forName("io.github.chanhyeong.workspace.chan.ISSUE100_getAverage");
     BaseExecutor obj = (BaseExecutor)cls.getConstructor().newInstance();
     obj.execute();
   }
--- src/main/java/io/github/chanhyeong/workspace/chan/ISSUE100_getAverage.java
+++ src/main/java/io/github/chanhyeong/workspace/chan/ISSUE100_getAverage.java
@@ -1,10 +1,26 @@
 package io.github.chanhyeong.workspace.chan;
 
 import io.github.chanhyeong.common.BaseExecutor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 public class ISSUE100_getAverage implements BaseExecutor {
   @Override
   public void execute() {
-    System.out.println("test");
+    Employee employee = new Employee();
+    employee.setName("테스트");
+    employee.setIncome(10000000L);
+
+    System.out.println(employee.getName());
+    System.out.println("Employee with lombok");
+  }
+
+  @Getter
+  @Setter
+  @ToString
+  private class Employee {
+    private String name;
+    private long income;
   }
 }
