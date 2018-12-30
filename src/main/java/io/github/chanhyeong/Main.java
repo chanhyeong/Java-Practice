276bdd333332d9ede083b33d3f02ce0d39f911b3
chanhyeong <chu7825@gmail.com>
2018-12-30 22:36:37 +0900

    add file handling features
    - get sub directories under "/data"
    - get files under "/data/" + "input directory name"

--- src/main/java/io/github/chanhyeong/Main.java
+++ src/main/java/io/github/chanhyeong/Main.java
@@ -8,6 +8,8 @@ public class Main {
     Class<?> cls = Class.forName("io.github.chanhyeong.workspace.chan.ISSUE100_getAverage");
     BaseExecutor obj = (BaseExecutor)cls.getConstructor().newInstance();
     obj.execute();
-    FileUtil.writeFile("writeFileTest", "writeFileTest", "w");
+    FileUtil.getSubDirectoryListInStorage().forEach(System.out::println);
+    FileUtil.writeFile("chanhyeong/writeFileTest.txt", "writeFileTest Text File", "w"); // chanhyeong 하위에 파일 쓰기
+    FileUtil.getFileListInDirectory("chanhyeong").forEach(System.out::println); // 파일이 저장되어있는지 확인
   }
 }
--- src/main/java/io/github/chanhyeong/util/FileUtil.java
+++ src/main/java/io/github/chanhyeong/util/FileUtil.java
@@ -1,9 +1,14 @@
 package io.github.chanhyeong.util;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileUtil {
 
@@ -19,6 +24,26 @@ public class FileUtil {
 	}
 
 	// TODO reflection 으로 프로퍼티 명 최상단에 붙이기
+
+	/**
+	 * 정의한 디렉토리(/data) 하위에 정의된 디렉토리 목록 가져오기
+	 * http://www.avajava.com/tutorials/lessons/how-do-i-get-the-directories-in-a-directory-using-a-filter.html
+	 *
+	 * @return 작업자 별 데이터 저장 경로
+	 */
+	public static List<String> getSubDirectoryListInStorage() {
+		File[] dirs = new File(FILE_NAME_PREFIX).listFiles((FileFilter) DirectoryFileFilter.DIRECTORY);
+		return Arrays.stream(dirs != null ? dirs : new File[] {})
+			.map(File::getName)
+			.collect(Collectors.toList());
+	}
+
+	public static List<String> getFileListInDirectory(String directoryName) {
+		return FileUtils.listFiles(new File(FILE_NAME_PREFIX + directoryName), TrueFileFilter.TRUE, null)
+			.stream()
+			.map(File::getName)
+			.collect(Collectors.toList());
+	}
 
 	private static boolean isAppend(String mode) {
 		return "a".equals(mode);
